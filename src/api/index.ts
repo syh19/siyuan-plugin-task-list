102c537d0d3b2d7cbba027213d8e76031b094476
xhir <syh1508@163.com>
2024-03-24 19:07:41 +0800

    修复仅已完成节点某些情况下无法正常显示的问题

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,30 +1,27 @@
 import * as sySDK from '@siyuan-community/siyuan-sdk'
+import type { TSqlResItem, TResponse } from '../types'
 
 /* 初始化客户端 (默认使用 Axios 发起 XHR 请求) */
 export const client = new sySDK.Client()
 
 /** 获取指定范围的taskNode节点列表 */
-export async function getTaskListBySql({
-  status,
-  boxId,
-  docId,
-}: {
+export async function getTaskListBySql(params: {
   status: string
   docId?: string
   boxId?: string
-}): Promise<any> {
+}): Promise<TResponse<Array<TSqlResItem>>> {
   let stmtStr = "SELECT * FROM blocks WHERE type = 'i' AND subtype = 't'"
 
-  if (status === 'todo') {
+  if (params.status === 'todo') {
     stmtStr += ` AND markdown LIKE '%[ ]%'`
-  } else if (status === 'done') {
-    stmtStr += ` AND markdown LIKE '%[x]%' AND markdown NOT LIKE '%[ ]%'`
-  } else if (status === 'all') {
+  } else if (params.status === 'done') {
+    stmtStr += ` AND markdown LIKE '%[_]%' AND markdown NOT LIKE '%[ ]%'` // 已完成可能有两种：[x] 和 [X]
+  } else if (params.status === 'all') {
     stmtStr += ''
   }
 
-  docId && (stmtStr += ` AND root_id = '${docId}'`)
-  boxId && (stmtStr += ` AND box = '${boxId}'`)
+  params.docId && (stmtStr += ` AND root_id = '${params.docId}'`)
+  params.boxId && (stmtStr += ` AND box = '${params.boxId}'`)
 
   stmtStr += ` ORDER BY created DESC LIMIT 1000`
 
