c32c9515b25ef7eb76ad73787ff67146ce779c07
xhir <syh1508@163.com>
2024-03-24 19:06:20 +0800

    添加sql接口返回值的类型定义

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,32 @@
 export type IRange = 'doc' | 'box' | 'workspace'
+
+export type TResponse<T> = {
+  code: number
+  data: T
+  msg: string
+}
+
+/** sql接口的返回值类型 */
+export type TSqlResItem = {
+  alias: string
+  box: string
+  content: string
+  created: string
+  fcontent: string
+  hash: string
+  hapth: string
+  ial: string
+  id: string
+  length: number
+  markdown: string
+  memo: string
+  name: string
+  parent_id: string
+  path: string
+  root_id: string
+  sort: number
+  subtype: string
+  tag: string
+  type: string
+  updated: string
+}
